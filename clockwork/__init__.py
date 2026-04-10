74d537c026401a7e15eb126c9aba5f8352b4574f
Zachary Einck <zacharyeinck@gmail.com>
2026-04-09 23:38:32 -0500

    chore: bump version to 0.6.0

--- clockwork/__init__.py
+++ clockwork/__init__.py
@@ -1,12 +1,12 @@
 from .timestamp import Timestamp
 from .month_end import MonthEnd
 from .quarter_end import QuarterEnd
-from .decorators import action_timer
+from .decorators import print_duration
 
 from .utils import (
     format_duration,
     temporal_format_to_regex,
     )
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = 'Zachary Einck <zacharyeinck@gmail.com>'
